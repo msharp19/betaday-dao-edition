5f09309e03124c615938d1e04c7dd6a8a8f3d075
Matt Sharp <msharp19@users.noreply.github.com>
2025-07-13 12:12:30 +0800

    Update ENSDaoAdapter.sol

--- ENSDaoAdapter.sol
+++ ENSDaoAdapter.sol
@@ -2,48 +2,96 @@
 pragma solidity ^0.8.0;
 
 import "./IDaoAdapter.sol";
+import "./IENSDaoAdapter.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract ENSDaoAdapter is IDaoAdapter
-{
+contract ENSDaoAdapter is IDaoAdapter, IENSDaoAdapter, Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable, AccessControlUpgradeable, UUPSUpgradeable 
+{   
+    bytes32 private constant AppStorageSlot = 0x098b9a5a10e60aff8f55e9477cc53791735a7ce2b851408e1eb5a144966fb300;
 
-    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) 
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        string memory daoName,
+        address daoAddress
+    ) public initializer {
+
+        __ReentrancyGuard_init();
+        __Ownable_init(_msgSender());
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+
+        AppStorage storage $ = _appStorage();
+        $.daoName = daoName;
+        $.daoAddress = daoAddress;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IDaoAdapter, IENSDaoAdapter, AccessControlUpgradeable) returns (bool) 
     {
-        /// @inheritdoc IDaoAdapter
-        return interfaceId == type(IDaoAdapter).interfaceId;
+        return
+            interfaceId == type(IDaoAdapter).interfaceId ||
+            interfaceId == type(IENSDaoAdapter).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
     function getDaoName() external view returns (string memory)
     {
-        /// @inheritdoc IDaoAdapter
+        AppStorage storage $ = _appStorage();
+        return $.daoName;
     }
 
     function getDaoAddress() external view returns (address)
     {
-        /// @inheritdoc IDaoAdapter
+        AppStorage storage $ = _appStorage();
+        return $.daoAddress;
     }
 
     function mapProposal(bytes[] memory nativeProposalId, uint256 mappedProposalId) external returns (bool)
     {
-        /// @inheritdoc IDaoAdapter
+        
     }
 
     function getOutcomeOfProposal(uint256 proposalId) external view returns (DaoOutcome)
     {
-        /// @inheritdoc IDaoAdapter
+        
     }
 
     function getProposalEndDate(uint256 proposalId) external view returns (uint256)
     {
-        /// @inheritdoc IDaoAdapter
+        
     }
 
     function proposalExists(uint256 proposalId) external view returns (bool)
     {
-        /// @inheritdoc IDaoAdapter
+        
     }
 
     function proposalExists(bytes[] memory nativeProposalId) external view returns (bool)
     {
-        /// @inheritdoc IDaoAdapter
+
     }
+
+    /**
+     * @dev Diamond storage pattern accessor.
+     * @return $ Reference to the AppStorage struct.
+     */
+    function _appStorage() private pure returns (AppStorage storage $) 
+    {
+        assembly { $.slot := AppStorageSlot }
+    }
+ 
+    /**
+     * @dev UUPS upgrade authorization hook.
+     * @notice Only owner can authorize upgrades.
+     */
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
