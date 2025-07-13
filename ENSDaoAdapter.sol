6a2f5bd25605a7ac0d7793a782f2d2da4172cc05
Matt Sharp <msharp19@users.noreply.github.com>
2025-07-13 21:38:09 +0800

    Update ENSDaoAdapter.sol

--- ENSDaoAdapter.sol
+++ ENSDaoAdapter.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/governance/IGovernor.sol";
 
 contract ENSDaoAdapter is IDaoAdapter, IENSDaoAdapter, Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable, AccessControlUpgradeable, UUPSUpgradeable 
 {   
@@ -37,8 +38,7 @@ contract ENSDaoAdapter is IDaoAdapter, IENSDaoAdapter, Initializable, OwnableUpg
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(IDaoAdapter, IENSDaoAdapter, AccessControlUpgradeable) returns (bool) 
     {
-        return
-            interfaceId == type(IDaoAdapter).interfaceId ||
+        return interfaceId == type(IDaoAdapter).interfaceId ||
             interfaceId == type(IENSDaoAdapter).interfaceId ||
             super.supportsInterface(interfaceId);
     }
@@ -55,29 +55,88 @@ contract ENSDaoAdapter is IDaoAdapter, IENSDaoAdapter, Initializable, OwnableUpg
         return $.daoAddress;
     }
 
-    function mapProposal(bytes[] memory nativeProposalId, uint256 mappedProposalId) external returns (bool)
+    function mapProposal(bytes memory nativeProposalId, uint256 mappedProposalId) external returns (bool)
     {
-        
+        AppStorage storage $ = _appStorage();
+
+        uint256 daoProposalId = _bytesToUint256(nativeProposalId);
+
+        $.nativeProposalMappings[mappedProposalId] = ProposalMapping(true, daoProposalId);
+
+        return true;
     }
 
     function getOutcomeOfProposal(uint256 proposalId) external view returns (DaoOutcome)
     {
-        
+        AppStorage storage $ = _appStorage();
+
+        ProposalMapping memory mappedProposal = $.nativeProposalMappings[proposalId];
+
+        IGovernor.ProposalState state = IGovernor($.daoAddress).state(mappedProposal.externalProposalId);
+
+        if(state == IGovernor.ProposalState.Active || state == IGovernor.ProposalState.Pending)
+        {
+            return DaoOutcome.Unresolved;
+        }
+
+        if(state == IGovernor.ProposalState.Executed || state == IGovernor.ProposalState.Queued || state == IGovernor.ProposalState.Succeeded)
+        {
+           return DaoOutcome.Succeeded;
+        }
+
+        if(state == IGovernor.ProposalState.Defeated)
+        {
+           return DaoOutcome.Defeated;
+        }
+
+        if(state == IGovernor.ProposalState.Canceled)
+        {
+           return DaoOutcome.Cancelled;
+        }
+
+        revert("Proposal outcome could not be mapped.");
     }
 
-    function getProposalEndDate(uint256 proposalId) external view returns (uint256)
+    function getProposalEndDate(uint256 proposalId) external view returns (uint256 deadline)
     {
-        
+        AppStorage storage $ = _appStorage();
+
+        ProposalMapping memory mappedProposal = $.nativeProposalMappings[proposalId];
+
+        require(mappedProposal.exists, "There is no mapped proposal for the ID specified");
+
+        deadline = IGovernor($.daoAddress).proposalDeadline(mappedProposal.externalProposalId);
     }
 
-    function proposalExists(uint256 proposalId) external view returns (bool)
+    function proposalIsRegistered(uint256 proposalId) external view returns (bool)
     {
-        
+        AppStorage storage $ = _appStorage();
+
+        ProposalMapping memory mappedProposal = $.nativeProposalMappings[proposalId];
+
+        return mappedProposal.exists;
     }
 
-    function proposalExists(bytes[] memory nativeProposalId) external view returns (bool)
+    function externalProposalExists(bytes memory nativeProposalId) external view returns (bool)
     {
+        AppStorage storage $ = _appStorage();
 
+        uint256 daoProposalId = _bytesToUint256(nativeProposalId);
+
+        // Reverts if there is no proposal registered ENS side for the provided ID
+        IGovernor($.daoAddress).state(daoProposalId);
+
+        return true;
+    }
+
+    function _bytesToUint256(bytes memory data) internal pure returns (uint256) 
+    {
+        require(data.length == 32, "Invalid bytes length for uint256 conversion");
+        uint256 num;
+        assembly {
+            num := mload(add(data, 0x20))
+        }
+        return num;
     }
 
     /**
